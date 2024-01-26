e7b3988560b4f519966b19cc502cede6ed3192c8
AliBazoubandi <alibazoubandi@gmail.com>
2024-01-26 16:05:49 +0330

    adding necessary comments for better understanding

---
+++ scripts/generateSecretKey.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+// Generate a random 32-character hexadecimal string
+const secretKey = crypto.randomBytes(16).toString('hex');
+
+fs.writeFileSync('.env', `SECRET_KEY=${secretKey}\n`);
+
+console.log('Secret key generated and saved!');
