18bc0bb77b13adf80f78bd2ab3d76b7e15e70598
AliBazoubandi <alibazoubandi@gmail.com>
2024-01-26 20:06:48 +0330

    added logout, follow, unfollow, and home page

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,9 @@ const authenticateUser = require('../middlewares/authenticateUser');
 
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
+router.post('/logout', authenticateUser, userController.logoutUser);
 router.get('/:userId', authenticateUser, userController.getUserById);
+router.post('/follow/:targetUserId', authenticateUser, userController.followUser);
+router.delete('/unfollow/:targetUserId', authenticateUser, userController.unfollowUser);
 
 module.exports = router;
